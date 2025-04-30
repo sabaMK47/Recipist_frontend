6416fd96aa250e1035052bcc8d29fa3a88c80ac7
msaba <msaba9405@gmail.com>
2025-04-30 16:36:33 +0330

    fonts, colors and navbar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,7 +1,20 @@
 <template>
+    <nav class="w-screen flex">
+        <div class="logo">
+            <h1 class="logo-font text-9xl text-main">Recipist</h1>
+        </div>
+        <ul class="list-none flex">
+            <li class="font-['text-medium', sans-serif]">menu</li>
+            <li>about us</li>
+        </ul>
+
+    </nav>
 
 </template>
 <script setup>
 </script>
-<style scoped>
+<style>
+    .logo-font{
+        font-family: 'logo-font' , sans-serif;
+    }
 </style>
