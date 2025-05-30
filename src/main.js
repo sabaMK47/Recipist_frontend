fd581e080c76f42b4761abd18138cacaac267e4b
maryam.khosro <118604201+sabaMK47@users.noreply.github.com>
2025-05-30 16:23:38 +0330

    turned the app to PWA and added oku motion

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { motionPlugin } from '@oku-ui/motion'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(motionPlugin)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,53 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'favicon.svg',
+        'favicon-96x96.png',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'Recipist',
+        short_name: 'Recipist',
+        description: 'Discover. Cook. Eat. Love. Repeat.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
