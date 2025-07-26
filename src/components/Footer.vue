ed6f420fe2de83dd4b792827b7ca6a127b485a3c
maryam.khosro <118604201+sabaMK47@users.noreply.github.com>
2025-07-26 20:19:50 +0330

    footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="bg-light dark:bg-darkgreen text-neutral-700 dark:text-neutral-300 px-6 py-10 w-screen">
-        <div class="mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-items-center">
+        <div class="mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center">
             <!-- Brand / About -->
             <div>
                 <h2 class="text-xl logo-font text-main">Recipist</h2>
@@ -14,8 +14,8 @@
                 <h3 class="font-semibold mb-2 text-font-medium text-main">Links</h3>
                 <ul class="space-y-1 text-sm text-font-light">
                     <li class="menu-li"><a href="/about">About Us</a></li>
-                    <li class="menu-li"><a href="/contact">Search by Ingredients</a></li>
-                    <li class="menu-li"><a href="/signup">Sign Up / Log In</a></li>
+                    <li class="menu-li"><a href="/">Search by Ingredients</a></li>
+                    <li class="menu-li"><a href="/auth">Sign Up / Log In</a></li>
                 </ul>
             </div>
 
@@ -23,27 +23,12 @@
             <div>
                 <h3 class="font-semibold mb-2 text-font-medium text-main">Recipe Categories</h3>
                 <ul class="space-y-1 text-sm text-font-light">
-                    <li class="menu-li"><a href="#">Appetizers</a></li>
-                    <li class="menu-li"><a href="#">Main Dishes</a></li>
-                    <li class="menu-li"><a href="#">Desserts</a></li>
-                    <li class="menu-li"><a href="#">Drinks</a></li>
+                    <li class="menu-li" @click="goToLink('appetizers')">Appetizers</li>
+                    <li class="menu-li" @click="goToLink('main-dish')">Main Dishes</li>
+                    <li class="menu-li" @click="goToLink('desserts')">Desserts</li>
+                    <li class="menu-li" @click="goToLink('vegan')">Vegan</li>
                 </ul>
-            </div>
-
-            <!-- Social / Install -->
-            <div>
-                <h3 class="font-semibold mb-2 text-font-medium text-main">Social Media</h3>
-                <div class="flex space-x-4 rtl:space-x-reverse gap-5 pt-5">
-                    <a href="#" class="text-blue-600 hover:text-blue-800">
-                        <img src="../assets/icons/facebook.svg" alt="facebook" class="w-6 h-6"> </a>
-                    <a href="#" class="text-blue-600 hover:text-blue-800">
-                        <img src="../assets/icons/instagram.svg" alt="instagram" class="w-6 h-6"> </a>
-                    <a href="#" class="text-blue-600 hover:text-blue-800">
-                        <img src="../assets/icons/telegram.svg" alt="telegram" class="w-6 h-6"> </a>
-                    <a href="#" class="text-blue-600 hover:text-blue-800">
-                        <img src="../assets/icons/twitter.svg" alt="twitter" class="w-6 h-6"> </a>
-                </div>
-            </div>
+            </div>      
         </div>
 
         <!-- Bottom note -->
@@ -52,3 +37,13 @@
         </div>
     </footer>
 </template>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goToLink = (tag) =>{
+    router.push({ path: '/recipes', query: { tags: tag } })
+     window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+</script>
